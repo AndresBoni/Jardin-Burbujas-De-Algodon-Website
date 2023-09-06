0432485a15c17b917a4205b2baf5f55a73ed275f
Andres Boni <andresboni91@gmail.com>
2023-09-07 02:55:00 +1200

    se ajusta el responsive de todas las secciones

--- frontend/src/components/Contacto.jsx
+++ frontend/src/components/Contacto.jsx
@@ -1,36 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Contacto = () => {
   return (
-
-
-    <>
-    <div classname='container section mt-5' id='Contacto'>
-      <div classnameN='row d-flex justify-content-center'>
-        <div classname='tittle-shape4'>
-          <div className='tittle d-flex text-nowrap'>
-            Contancto
-          </div>
-        </div>
+    <div className="container section" id="contacto">
+      <div className="row d-flex justify-content-center">
+        <div className="col-auto title">Contacto</div>
       </div>
-
     </div>
+  );
+};
 
-
-    {/* formulario */}
-
-
-    
-
-
-
-
-
-
-
-   </>
-
-  )
-}
-
-export default Contacto
+export default Contacto;
--- frontend/src/components/Programas.jsx
+++ frontend/src/components/Programas.jsx
@@ -1,76 +1,40 @@
-import React from 'react'
-import "./styles/Programas.css"
-import rectangleazul from '../assets/img/Rectangle 7.jpg'
-import rectangleverde from '../assets/img/Rectangle 9.jpg'
-import rectanglerosa from '../assets/img/Rectangle 8.jpg'
-
-
+import React from "react";
+import "./styles/Programas.css";
+import imgRectangleTeal from "../assets/img/imgRectangleTeal.png";
+import imgRectanglePink from "../assets/img/imgRectanglePink.png";
+import imgRectangleYellow from "../assets/img/imgRectangleYellow.png";
 
 const Programas = () => {
   return (
-  
-  <>
-   
-   <div className='container section' id='Programas'>
-      <div className='row d-flex justify-content-center'>
-        <div className='tittle-shape1'>
-          <div className='tittle d-flex text-nowrap'>
-            Programas
+    <div className="container section" id="programas">
+      <div className="row d-flex justify-content-center">
+        <div className="col-auto title">Programas Educativos</div>
+      </div>
+      <div class="row d-flex">
+        <div class="col-md-3 d-flex flex-column justify-content-evenly ">
+          <span className="encabezado-texto-programas">
+            Mira nuestros Programas
+          </span>
+          <p className="texto-programas">
+            ¡Bienvenidos a nuestro cálido y estimulante jardín de infantes!
+            Aquí, cada niño es una semilla lista para crecer y florecer en un
+            ambiente educativo lleno de alegría y descubrimiento.
+          </p>
+        </div>
+        <div class="col-md-9 d-flex justify-content-evenly">
+          <div className="col-3">
+            <img src={imgRectangleTeal} className="img-fluid" alt="" />
+          </div>
+          <div className="col-3">
+            <img src={imgRectanglePink} className="img-fluid" alt="" />
+          </div>
+          <div className="col-3">
+            <img src={imgRectangleYellow} className="img-fluid" alt="" />
           </div>
         </div>
       </div>
-
     </div>
+  );
+};
 
-
-
-
-     
-     
-    
-    <div class="row centrado mt-5 ms-5">
-    <div class="col-md-4 texto-fuente ">
-
-    <h2>Mira nuestros Programas</h2>
-    <p>¡Bienvenidos a nuestro cálido y estimulante jardín de infantes! Aquí, cada niño es una semilla lista para crecer y florecer en un ambiente educativo lleno de alegría y descubrimiento.</p>
-
-
-
-
-    </div>
-    <div class="col-md-4 offset-md-4 imagenes-colores">
-       
-
-      <div className='separacion-imagen tamaño-imagen'>
-                           <img src={rectangleverde} className='rounded float-start x ms-4 ' alt="" />
-                           <img src={rectangleazul} className='rounded float-start x ms-4' alt="" />
-                           <img src={rectanglerosa} className='rounded float-start x ms-4' alt="" />
-      </div>
-      
-      
-      
-      
-    
-
-      </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-    </>
-  )
-}
-
-export default Programas
+export default Programas;
