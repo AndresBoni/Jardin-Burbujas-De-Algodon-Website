10a32203ab80bdbcb8059a5cf457d3402071ab74
Andres Boni <andresboni91@gmail.com>
2023-09-21 19:33:05 +1200

    routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const basePath = "/Jardin-Burbujas-De-Algodon-Website";
 
-export default Login
+  return (
+    <div className="container section" id="login">
+      <div className="row d-flex justify-content-center ">
+        <div className="col-auto title">Login</div>
+      </div>
+      <div className="row d-flex flex-column align-items-center justify-content-center">
+        <div className="col-12 col-lg-8">
+          <div className="d-flex flex-column gap-4">
+            <input
+              type="email"
+              className="form-control formulario"
+              id="emailInput"
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              className="form-control formulario"
+              id="passwordInput"
+              placeholder="Contraseña"
+            />
+            <Link to={`${basePath}/dashboard`}>
+              <button className="form-control btn-enviar">Ingresar</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
